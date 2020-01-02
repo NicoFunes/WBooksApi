6e8f528372c0227434ce528bfd9c7924c9f3af97
NicoFunes <nicolas.funes@widergy.com>
2020-01-02 13:02:36 -0300

    Add space and correct indentation

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -2,6 +2,7 @@ module User
   class RegistrationsController < Devise::RegistrationsController
     skip_before_action :verify_authenticity_token
     respond_to :json
+
     def create
       build_resource(sign_up_params)
       resource.save
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,9 @@
 module User
   class SessionsController < Devise::SessionsController
     respond_to :json
+
+    private
+
     def respond_with(resource, _opts = {})
       render json: resource
     end
