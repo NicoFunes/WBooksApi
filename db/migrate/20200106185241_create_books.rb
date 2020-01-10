79a2e0f3f4aef43f3bbe8ea50a80248e7e96eb8b
NicoFunes <nicolas.funes@widergy.com>
2020-01-10 14:37:13 -0300

    Change books columns names and remove files

--- db/migrate/20200106185241_create_books.rb
+++ db/migrate/20200106185241_create_books.rb
@@ -1,12 +1,12 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string :Title, null: false, default: ''
-      t.string :Author, null: false, default: ''
-      t.string :Image, null: false, default: ''
-      t.string :Genre, null: false, default: ''
-      t.string :Publisher, null: false, default: ''
-      t.string :Year, null: false, default: ''
+      t.string :title, null: false, default: ''
+      t.string :author, null: false, default: ''
+      t.string :image, null: false, default: ''
+      t.string :genre, null: false, default: ''
+      t.string :publisher, null: false, default: ''
+      t.string :year, null: false, default: ''
     end
   end
 end
