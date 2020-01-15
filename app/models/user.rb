a2a8e08592c2f0396441f923e7b7ecdc0716aa49
NicoFunes <58991676+NicoFunes@users.noreply.github.com>
2020-01-15 15:49:30 -0300

    Testing books model (#7)
    * Add simple testing examples
    
    * Fixing the failed Circle CI execute
    
    * Upload more test about books
    
    * Correction of the book test to use factory
    
    * Delete empty files and book controller
    
    * remove book controller spec file
    
    * Delete files generated from another branch
    
    * remove books collection helper file
    
    * Correction in user test
    
    * Correction of do it in one line

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates_presence_of :first_name, :email
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :database_authenticatable,
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it do
+    should validate_presence_of(:first_name)
+  end
+
+  subject(:user) do
+    create(:user)
+  end
+
+  it { is_expected.to be_valid }
+end
