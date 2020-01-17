d391975f35ed1cb5fdf33c1f5ae2dbb3e79dd24a
NicoFunes <nicolas.funes@widergy.com>
2020-01-17 15:12:17 -0300

    remove comments from file

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,13 +2,12 @@ class BooksController < ApplicationController
   respond_to :json
   skip_before_action :verify_authenticity_token
   before_action :authorize_request
-  # Muestra uno solo
+
   def show
     @abook = Book.find_by(id: params[:id])
     logger.debug "A book: #{@abook.title}"
   end
 
-  # Muestra todos
   def index
     @books = Book.all
   end
